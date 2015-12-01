d4fe14607b24a870993f422fdb24d37d32e78987
jedepaepe <jedepaepe@PC8823.domepfc.net>
2015-12-01 19:35:49 +0100

    Change code, add "hello git"

--- Lesson11/src/epfc/cours3449/lesson11.java
+++ Lesson11/src/epfc/cours3449/lesson11.java
@@ -17,6 +17,7 @@ public class lesson11 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Git");
     }
     
 }
