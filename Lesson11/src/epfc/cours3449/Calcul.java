d45609967de03474a0e367c028b3dcbe8d4adfa3
jedepaepe <jedepaepe@PC8823.domepfc.net>
2015-12-01 22:00:31 +0100

    Good design : static method

--- Lesson11/src/epfc/cours3449/Calcul.java
+++ Lesson11/src/epfc/cours3449/Calcul.java
@@ -1,12 +1,11 @@
 // livre page 316
-// mauvais design de factoriel : méthode non dépendante d'un attribut de l'instance 
-// donc devrait être static.
+// static.
 //
 package epfc.cours3449;
 
 public class Calcul {
 
-    public int factorial(int n) {
+    static public int factorial(int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
@@ -15,9 +14,7 @@ public class Calcul {
     }
     
     public static void main(String[] args) {
-        Calcul c = new Calcul();
-        System.out.println(c.factorial(3));
-        // plus court mais ... pas très lisible
-        System.out.println( new Calcul().factorial(4) );
+        System.out.println(Calcul.factorial(3));
+        System.out.println( Calcul.factorial(4) );
     }
 }
