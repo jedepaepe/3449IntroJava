be34b4500430299838e120bdedebbdb0f183531f
jedepaepe <jedepaepe@PC8823.domepfc.net>
2015-12-01 20:47:19 +0100

    Code factorisé

--- Lesson11/src/epfc/cours3449/TestRandom.java
+++ Lesson11/src/epfc/cours3449/TestRandom.java
@@ -1,23 +1,20 @@
 /*
-8.6.2 The Random Class
-page 309
-*/
-
+ 8.6.2 The Random Class
+ page 309
+ */
 package epfc.cours3449;
 
 import java.util.Random;
 
 public class TestRandom {
+
     public static void main(String[] args) {
-        Random r1 = new Random(3);
-        for(int i=0; i<10; i++) {
-            System.out.print(r1.nextInt(1000) + " ");
+        for (int j = 0; j < 2; j++) {
+            Random r1 = new Random(3);
+            for (int i = 0; i < 10; i++) {
+                System.out.print(r1.nextInt(1000) + " ");
+            }
+            System.out.println("");
         }
-        System.out.println("");
-        Random r2 = new Random(3);
-        for(int i=0; i<10; i++) {
-            System.out.print(r2.nextInt(1000) + " ");
-        }
-        System.out.println("");
     }
 }
