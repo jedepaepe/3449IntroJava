3a18b71b55feb5e890d07cd8c99cde93da13fd28
jedepaepe <jedepaepe@PC8823.domepfc.net>
2016-01-12 22:00:50 +0100

    Relation votre code <=> librairie: JavaFX librairy peut appeler les méthodes de votre code car votre code hérite de la classe JavaFX...Application

---
+++ cours/src/eu/epfc/cours3449/l15/Part2Abstract/TestJavaFx.java
@@ -0,0 +1,22 @@
+
+package eu.epfc.cours3449.l15.Part2Abstract;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jedepaepe
+ */
+public class TestJavaFx extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
