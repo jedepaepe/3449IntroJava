b6983e0149ab6477bb70c47fd4c65ffcd708f5ce
jedepaepe <jedepaepe@PC8856.domepfc.net>
2016-02-02 20:57:34 +0100

    Encodage via la console de livres et écriture sur le fichier "livres.txt"

---
+++ cours/src/eu/epfc/cours3449/l18/p2Bibliotheque/GestionLivres.java
@@ -0,0 +1,35 @@
+package eu.epfc.cours3449.l18.p2Bibliotheque;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestionLivres {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner keyb = new Scanner(System.in);
+        boolean bNext = true;
+        ArrayList<Livre> livres = new ArrayList<>();
+        
+        do {
+            Livre l = new Livre();
+            livres.add(l);
+            System.out.print("Identifiant: ");
+            l.setIdentifiant(keyb.nextLine());
+            System.out.print("Place: ");
+            l.setPlace(keyb.nextLine());
+            System.out.print("Data d'achat: ");
+            l.setDateAchat(keyb.nextLine());
+            System.out.print("\"N\" stoppe l'encodage, autre caractère continue: ");
+            if( "N".equals(keyb.nextLine()) ) bNext = false;
+        } while (bNext);
+        
+        PrintWriter out = new PrintWriter("livres.txt");
+        for(int i=0; i<livres.size(); i++) {
+            System.out.println(livres.get(i));
+            out.println(livres.get(i));
+        }
+        out.close();
+    }
+}
